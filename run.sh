0403e15b46a4a6480fd2c3c3cbffe8ac6ecf2cfd
LisaMc <lmcferri@fredhutch.org>
2017-10-17 17:57:40 -0700

    update to v2 format

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-if [ -z "$MONGO_URL" ]; then
-    echo "Need to set MONGO_URL when calling this container! Exiting."
+if [ -z "$MONGO_CONNECTION" ]; then
+    echo "Need to set MONGO_CONNECTION when calling this container! Exiting."
     exit 1
 fi
 
+if [ -z "$MONGO_USERNAME" ]; then
+    echo "Need to set MONGO_USERNAME when calling this container! Exiting."
+    exit 1
+fi
 
+if [ -z "$MONGO_PASSWORD" ]; then
+    echo "Need to set MONGO_PASSWORD when calling this container! Exiting."
+    exit 1
+fi
 
 gunicorn -w 4 -b "0.0.0.0:8000" flask_app:app
